4aef7f1b8b2f60ba58a452c994d79335cc24bdd0
walidmeguenni <walidmg125@gmail.com>
2021-10-17 16:11:00 +0100

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout/Layout";
+import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
