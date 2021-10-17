563162413df2e3fd589010cd738cd9800bf8f5f6
walidmeguenni <walidmg125@gmail.com>
2021-10-17 15:08:16 +0100

    fix wabpack5 fail

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  // Webpack 5 is enabled by default
-  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-  webpack5: false,
-}
+  future: { webpack5: true },
+};
